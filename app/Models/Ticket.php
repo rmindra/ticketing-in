4e678adf448483ba39bb733d2cb0cc4ab024a429
rmindra <indraaa.rm@gmail.com>
2025-11-23 11:14:00 +0700

    Initial, Migration, and Installing AdminLTE template

---
+++ app/Models/Ticket.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    protected $fillable = ['title', 'description', 'priority', 'status', 'user_id', 'category_id', 'assigned_to'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function technician()
+    {
+        return $this->belongsTo(User::class, 'assigned_to');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
